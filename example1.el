99c9a91f5b0b2a6947958ab31dedf4c1494549f2
Chad Crowe <ccrowe@linkedin.com>
2019-10-09 10:29:46 -0500

    adding examples for manipulating files

--- example1.el
+++ example1.el
@@ -46,3 +46,58 @@
 (add-hook 'find-file-hook 'highlight-it)
 
 
+                                        ; Important variables
+(point)
+(region-beginning)
+(region-end)
+
+(line-beginning-position)
+(line-end-position)
+
+(point-min) ; in buffer
+(point-max)
+
+                                        ; movement
+(goto-char 39)
+(forward-char 4)
+(backward-char 4)
+
+(search-forward "some")
+(search-backward "some")
+
+(re-search-forward "[0-9]")
+(re-search-backward "[0-9]")
+
+                                        ; move cursor to first no in provided list
+(skip-chars-foward "a-z")
+(skip-chars-backward "a-z")
+
+                                        ; delete 9 chars from current point
+(delete-char 9)
+                                        ; delete text from pos 3 to 10
+(delete-region 3 10)
+                                        ;insert string
+(insert "I love dogs")
+                                        ; get the string from position 71 to 300
+(setq x (buffer-substring 7 300))
+
+                                        ; buffer variables
+(buffer-name)
+(buffer-file-name)
+                                        ;switch to the buffer with this name
+(set-buffer "xyz")
+(save-buffer)
+(kill-buffer "xyz")
+(with-current-buffer "xyz"
+                                        ; do something
+  )
+
+; open file in buffer
+(find-file "~/")
+; same as save as
+(write-file path)
+
+(insert-file-contents path) ; insert file into current path
+
+(append-to-file start-pos end-pos path); insert from start to end to the file at that path
+
